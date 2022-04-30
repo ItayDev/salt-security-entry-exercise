a0bb7f11986a4ee1af8dd660bbf08ce6ffdf0316
Itay Pinhas <itay@pinhas.org>
2022-04-30 15:02:54 +0300

    Made field static

--- src/main/java/org/maybe/salt/assignment/validation/validator/DateValidator.java
+++ src/main/java/org/maybe/salt/assignment/validation/validator/DateValidator.java
@@ -5,17 +5,15 @@ import org.apache.commons.validator.GenericValidator;
 import org.maybe.salt.assignment.models.FieldType;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Optional;
 
 @Component
 public class DateValidator implements TypeValidator {
-    private final String dateFormat = "dd-MM-yyyy";
+    private final static String DATE_FORMAT = "dd-MM-yyyy";
 
     @Override
     public Optional<TypeValidationError> validate(JsonNode fieldValue) {
-        if(GenericValidator.isDate(fieldValue.asText(), this.dateFormat, true)) {
+        if(GenericValidator.isDate(fieldValue.asText(), DATE_FORMAT, true)) {
             return Optional.empty();
         }
 
